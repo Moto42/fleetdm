7bdb415cf7c4f9f15e4e8280f859de2f8a7e08d5
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2024-08-26 19:23:08 -0500

    use printf not echo

--- certmaker/checkforcerts.sh
+++ certmaker/checkforcerts.sh
@@ -10,10 +10,10 @@ bothfilesexist(){
 }
 
 if bothfilesexist; then
-  echo "################\nAll certs exist\n################"
+  printf "################\nAll certs exist\n################"
   exit 0
 else
-  echo "##########\nSome or all certs not found\n(re)generating certs\n##########"
+  printf "##########\nSome or all certs not found\n(re)generating certs\n##########"
   rm /certs/*
   /usr/local/bin/generate-certs || exit 1
   mv /certs/cert.pem /certsvolume/cert.pem
