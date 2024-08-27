2e525fa4a2e28056ab05d524bc1f2d4faad93bb3
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2024-08-26 19:20:06 -0500

    add status messages for certmaker service

--- certmaker/checkforcerts.sh
+++ certmaker/checkforcerts.sh
@@ -10,10 +10,10 @@ bothfilesexist(){
 }
 
 if bothfilesexist; then
-  echo "thinger"
+  echo "################\nAll certs exist\n################"
   exit 0
 else
-  echo "thanger"
+  echo "##########\nSome or all certs not found\n(re)generating certs\n##########"
   rm /certs/*
   /usr/local/bin/generate-certs || exit 1
   mv /certs/cert.pem /certsvolume/cert.pem
